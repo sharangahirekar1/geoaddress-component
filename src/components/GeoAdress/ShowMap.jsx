5658da5b96b0f948a47fd9080bd845b66bbf368f
Sharang Ahirekar <sharang.ahirekar@exaltaretech.com>
2024-03-19 15:06:49 +0530

    adding dummy image on map as overloay

--- src/components/GeoAdress/ShowMap.jsx
+++ src/components/GeoAdress/ShowMap.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components';
-import { Map, Marker } from "pigeon-maps";
+import { Map, Marker, Overlay } from "pigeon-maps";
 import { osm } from 'pigeon-maps/providers';
 import axios from 'axios';
 import { getLocation, ReverseGeoCoding } from '../../store/GeoAddress/GeoAddress.action';
@@ -28,6 +28,9 @@ const ShowMap = () => {
     <Container id='map' style={{backgroundColor:"grey",width:"100%",height:"100vh"}} >
       <Map provider={osm} height={600} defaultCenter={[latiV,longV]} defaultZoom={11}>
         <Marker width={50} anchor={[latiV,longV ]} />
+        <Overlay anchor={[latiV,longV]} offset={[120, 79]}>
+          <img src='/logo512.png' width={240} height={158} alt='' />
+        </Overlay>
       </Map>
     </Container>
   )
